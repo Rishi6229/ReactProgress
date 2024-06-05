980d46a7ebfdcf7d97d9e2d34b4f690c007ef8fa
Rishi <rk6180442@gmail.com>
2024-06-05 21:28:30 +0530

    Day 1 react basic

---
+++ reactlessson/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a picture of a cat</h1>
+      <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="catphoto"/>
+    </div>
+  );
+}
+
+export default App;
